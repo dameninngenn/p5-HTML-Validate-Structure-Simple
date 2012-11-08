10a4c240b631844bbe5657ac0cc16356a506cbb0
dameninngenn <dameninngenn.owata@gmail.com>
2012-11-08 15:54:21 +0900

    initial commit

---
+++ lib/HTML/Validate/Structure/Simple.pm
@@ -0,0 +1,32 @@
+package HTML::Validate::Structure::Simple;
+use strict;
+use warnings;
+our $VERSION = '0.01';
+
+1;
+__END__
+
+=head1 NAME
+
+HTML::Validate::Structure::Simple -
+
+=head1 SYNOPSIS
+
+  use HTML::Validate::Structure::Simple;
+
+=head1 DESCRIPTION
+
+HTML::Validate::Structure::Simple is
+
+=head1 AUTHOR
+
+dameninngenn E<lt>dameninngenn.owata {at} gmail.comE<gt>
+
+=head1 SEE ALSO
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
